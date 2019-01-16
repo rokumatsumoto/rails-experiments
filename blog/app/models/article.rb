dc6b5f11f159aac3c3e370c0a254e8009dd1faa7
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-01-17 00:59:29 +0300

    Add blog project

---
+++ blog/app/models/article.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  text       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Article < ApplicationRecord
+  include ActiveModel::Serializers::Xml
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: {minimum: 5}
+
+
+  def test
+    return 'xsdfssfdf'
+  end
+end
