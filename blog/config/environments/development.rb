5c5ac772ff9e47e044df9f0a244ea142a9144ce6
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-02-07 22:56:18 +0300

    Disallow id in the article routes

--- blog/config/environments/development.rb
+++ blog/config/environments/development.rb
@@ -65,5 +65,5 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
